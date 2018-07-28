e9d2dcdbc1f9fe699d1799e45f24c94a1b05377d
Drew Thomas <cdthomas.92@gmail.com>
2018-07-28 17:33:07 +1000

    Add GraphiQL

--- shells/webextension/src/panel.js
+++ shells/webextension/src/panel.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import GraphiQL from "graphiql";
+import { host, headers as configHeaders } from "./requestConfig";
+import "graphiql/graphiql.css";
+
+function graphQLFetcher(graphQLParams) {
+  // TODO: make headers, host, path, and credentials configurable
+
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    ...configHeaders
+  };
+
+  return window
+    .fetch(`${host}/graphql`, {
+      method: "POST",
+      headers,
+      credentials: "same-origin",
+      body: JSON.stringify(graphQLParams)
+    })
+    .then(response => response.json());
+}
 
 class App extends Component {
   render() {
-    return <h1>Hello from React!</h1>;
+    return (
+      <div className="wrapper">
+        <GraphiQL fetcher={graphQLFetcher} />
+      </div>
+    );
   }
 }
 
-render(<App />, document.getElementById("react-app"));
+render(<App />, document.getElementById("app"));
--- shells/webextension/webpack.config.js
+++ shells/webextension/webpack.config.js
@@ -21,7 +21,16 @@ module.exports = {
         test: /\.js$/,
         use: "babel-loader",
         exclude: /(node_modules)/
+      },
+      // Workaround for .mjs files in graphql-js not working with Webpack 4
+      // See: https://github.com/graphql/graphql-js/issues/1272
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
       }
     ]
-  }
+  },
+  //  Workaround for https://github.com/graphql/graphql-language-service/issues/128
+  plugins: [new webpack.IgnorePlugin(/\.flow$/)]
 };
